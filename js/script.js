9fcae327ac0a1cdccdc8e0d58383d01ae6975eed
Laura DiSanza <ldisanza7@gmail.com>
2021-08-12 11:27:15 -0400

    Random time and holes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 const holes = document.querySelectorAll('.hole');
 const scoreBoard = document.querySelector('.score');
 const moles = document.querySelectorAll('.mole');
+
+//Random amount of time between moles popping up
+function randTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+//Random hole that the mole pops out of
+function randomHole(holes) {
+    
+}
